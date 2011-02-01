2def0ddab9863c5e3c166b3f708a547c0f9b8af0
camurca.home <camurca.home@fc72ca21-d713-2d2c-2c02-1a869acf1610>
2011-02-01 18:05:03 +0000

    > iniciado o about us, falta implementação.

--- trunk/rs-online/db/migrate/20110114023715_create_statuses.rb
+++ trunk/rs-online/db/migrate/20110114023715_create_statuses.rb
@@ -4,14 +4,6 @@ class CreateStatuses < ActiveRecord::Migration
 			t.column :status, :string, :null => false, :unique => true
 			t.timestamps
 		end
-
-		# default statuses
-		Status.create :status => "downloaded"
-		Status.create :status => "offline"
-		Status.create :status => "online"
-		Status.create :status => "downloading"
-		Status.create :status => "waiting"
-		Status.create :status => "interrupted"
 	end
 
 	def self.down
---
+++ trunk/rs-online/db/migrate/20110201153251_create_abouts.rb
@@ -0,0 +1,17 @@
+class CreateAbouts < ActiveRecord::Migration
+  def self.up
+    create_table :abouts do |t|
+      t.string :name, :null => false
+      t.text :bio
+      t.string :email
+      t.string :twitter
+      t.string :website
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :abouts
+  end
+end
