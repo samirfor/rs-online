6442eaf4c5ed1d781c7b80932990e3d825487298
camurca.home <camurca.home@fc72ca21-d713-2d2c-2c02-1a869acf1610>
2011-03-09 21:31:30 +0000

    > update em megaupload module. regex downloadlink! > novo module Message. mensagens padrão do sistema.

---
+++ rs-online-core/src/message.rb
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+module Message
+    def package_details id, nome
+      "#: #{id}, Pacote: #{nome}"
+    end
+end
